27f73cbf79ee82b660834c5ffe2bd4b3da0ef0d2
Colourless <205353678+AcrylonitrileButadieneStyrene@users.noreply.github.com>
2025-12-27 13:52:40 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# YNO Commumojis
+Unofficial community created emojis for [YNO](ynoproject.net)
+
+## Installation
+1. Add a userscript manager extension (e.g. 
+    [ViolentMonkey](https://violentmonkey.github.io/)
+    ([Chrome](https://chromewebstore.google.com/detail/violentmonkey/jinjaccalgkegednnccohejagnlnfdag)
+    / [Edge](https://microsoftedge.microsoft.com/addons/detail/eeagobfjdenkkddmbclomhiblgggliao)
+    / [Firefox](https://addons.mozilla.org/en-US/firefox/addon/violentmonkey)))
+2. Install [the script](https://raw.githubusercontent.com/AcrylonitrileButadieneStyrene/yno-commumoji/master/yno-commumoji.user.js)
+    - Opening the link should open an installation dialog.
+      
+      If it does not then manually create a new script and paste the contents into it.
+3. Reload YNO if it is already opened
+
+## Usage
+- Commumojis start with a `-` in their names (e.g. `:-emoji:`)
+  to not cause collisions in the future and to indicate them as 3rd party
+- The list of commumojis tries to update daily.
+- The last update time can be seen inside of the userscript manager's extension UI.
+  - Clicking on it will force a refresh.
+    Do not do this too often or you will be rate-limited.
+
+## Contributing
+
---
+++ yno-commumoji.user.js
@@ -0,0 +1,130 @@
+// ==UserScript==
+// @name        YNO Commumoji
+// @match       *://ynoproject.net/*
+// @version     0.1.0
+// @description Unofficial community created emojis for YNO
+// @noframes
+// @grant       GM_registerMenuCommand
+// @grant       GM_unregisterMenuCommand
+// @downloadURL https://raw.githubusercontent.com/AcrylonitrileButadieneStyrene/yno-commumoji/master/yno-commumoji.user.js
+// @supportURL  https://github.com/AcrylonitrileButadieneStyrene/yno-commumoji/issues
+// @homepageURL https://github.com/AcrylonitrileButadieneStyrene/yno-commumoji/blob/master/README.md
+// ==/UserScript==
+
+const configUrl = "https://raw.githubusercontent.com/AcrylonitrileButadieneStyrene/yno-commumoji/master/config.csv";
+const cache = unsafeWindow.caches.open("yno-commumoji");
+const maxAge = 86400000; // 1 day
+
+async function fetchConfiguration() {
+    const cached = await (await cache).match(configUrl);
+    if (cached && new Date() - new Date(cached.headers.get("Date")) < maxAge)
+        return await cached.text();
+
+    const response = await fetch(configUrl);
+    const status = response.status;
+    const statusText = response.statusText;
+    if (status >= 400)
+        throw statusText;
+    const headers = new Headers(response.headers);
+    const text = await response.text();
+
+    let date = new Date(headers.get("Date"))
+    if (isNaN(date.getTime())) {
+        date = new Date();
+        headers.set("Date", date.toUTCString());
+    }
+
+    await (await cache).put(configUrl, new Response(text, { status, statusText, headers }));
+    updateStatus(date);
+    return text;
+}
+
+function updateStatus(date) {
+    GM_unregisterMenuCommand("last-updated");
+    GM_registerMenuCommand(
+        "Last updated: " + date.toLocaleTimeString(undefined, { hour: 'numeric', minute: '2-digit', }),
+        async () => {
+            (await cache).delete(configUrl);
+            updateConfiguration();
+        },
+        { id: "last-updated" }
+    );
+}
+
+let ready = false;
+const ynomojis = new Promise(res => {
+    const original = Object.keys;
+    Object.keys = function (object) {
+        const keys = original.apply(this, arguments);
+        if (object[keys[0]] == keys[0] + ".png") {
+            Object.keys = original;
+            ready = true;
+            res(ynomojiConfig);
+        }
+        return keys;
+    }
+});
+
+let enqueuedConfiguration;
+(async function updateConfiguration() {
+    const config = await fetchConfiguration().split("\n")
+        .map(line => line.split(","))
+        .filter(pairs => pairs.length == 2)
+        .reduce((accumulator, [key, value]) => (accumulator[key] = value, accumulator), {});
+    const killswitch = config["#killswitch"];
+    if (killswitch) return alert(killswitch);
+
+    if (!ready) enqueuedConfiguration = config;
+    else applyConfig(await ynomojis, config);
+})();
+
+ynomojis.then(ynomojiConfig => {
+    if (enqueuedConfiguration) {
+        applyConfig(ynomojiConfig, enqueuedConfiguration)
+        enqueuedConfiguration = undefined;
+    }
+
+    // fix rendering in chat
+    patchWrapper();
+});
+
+async function applyConfig(ynomojis, config) {
+    const container = document.getElementById('ynomojiContainer');
+    for (const [key, url] of Object.entries(config)) {
+        if (ynomojis[key] == url) continue;
+
+        // add the custom emoji
+        ynomojis[key] = url;
+
+        // add to the emoji picker
+        addEmoji(container, key, url);
+    }
+}
+
+function addEmoji(container, key, url) {
+    // from play.js:2822 with slight changes
+    const ynomoji = document.createElement('img');
+    ynomoji.src = url;
+    addTooltip(ynomoji, `:${key}:`, true);
+    ynomoji.classList.add('ynomoji');
+    ynomoji.onclick = () => insertYnomoji(key);
+
+    const ynomojiButton = document.createElement('a');
+    ynomojiButton.href = 'javascript:void(0)';
+    ynomojiButton.dataset.ynomojiId = key;
+    ynomojiButton.classList.add('ynomojiButton');
+    ynomojiButton.appendChild(ynomoji);
+
+    container.appendChild(ynomojiButton);
+}
+
+function patchWrapper() {
+    const original = unsafeWindow.wrapMessageEmojis;
+    unsafeWindow.wrapMessageEmojis = function (node) {
+        original.apply(this, arguments);
+
+        const images = node.querySelectorAll(".ynomojiWrapper img");
+        for (const image of images)
+            image.src = image.src.substring(image.src.lastIndexOf("https://"))
+    }
+}
