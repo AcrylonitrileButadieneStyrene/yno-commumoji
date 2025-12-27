7d3aa37a583a71b246c66a8d6db2bd66ff711989
Colourless <205353678+AcrylonitrileButadieneStyrene@users.noreply.github.com>
2025-12-27 14:28:00 -0500

    remove test image and refactor

--- yno-commumoji.user.js
+++ yno-commumoji.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        YNO Commumoji
 // @match       *://ynoproject.net/*
-// @version     0.1.0
+// @version     0.1.1
 // @description Unofficial community created emojis for YNO
 // @noframes
 // @grant       GM_registerMenuCommand
@@ -12,11 +12,11 @@
 // ==/UserScript==
 
 const configUrl = "https://raw.githubusercontent.com/AcrylonitrileButadieneStyrene/yno-commumoji/master/config.csv";
-const cache = unsafeWindow.caches.open("yno-commumoji");
 const maxAge = 86400000; // 1 day
 
 async function fetchConfiguration() {
-    const cached = await (await cache).match(configUrl);
+    const cache = await unsafeWindow.caches.open("yno-commumoji");
+    const cached = await cache.match(configUrl);
     if (cached && new Date() - new Date(cached.headers.get("Date")) < maxAge)
         return await cached.text();
 
@@ -28,27 +28,25 @@ async function fetchConfiguration() {
     const headers = new Headers(response.headers);
     const text = await response.text();
 
-    let date = new Date(headers.get("Date"))
-    if (isNaN(date.getTime())) {
+    let date = headers.get("Date");
+    if (date) date = new Date(date)
+    if (!date || isNaN(date.getTime()))
         date = new Date();
-        headers.set("Date", date.toUTCString());
-    }
 
-    await (await cache).put(configUrl, new Response(text, { status, statusText, headers }));
-    updateStatus(date);
-    return text;
-}
+    headers.set("Date", date.toUTCString());
 
-function updateStatus(date) {
     GM_unregisterMenuCommand("last-updated");
     GM_registerMenuCommand(
         "Last updated: " + date.toLocaleTimeString(undefined, { hour: 'numeric', minute: '2-digit', }),
-        async () => {
-            (await cache).delete(configUrl);
+        () => {
+            cache.delete(configUrl);
             updateConfiguration();
         },
         { id: "last-updated" }
     );
+
+    await cache.put(configUrl, new Response(text, { status, statusText, headers }));
+    return text;
 }
 
 let ready = false;
@@ -67,7 +65,7 @@ const ynomojis = new Promise(res => {
 
 let enqueuedConfiguration;
 (async function updateConfiguration() {
-    const config = await fetchConfiguration().split("\n")
+    const config = (await fetchConfiguration()).split("\n")
         .map(line => line.split(","))
         .filter(pairs => pairs.length == 2)
         .reduce((accumulator, [key, value]) => (accumulator[key] = value, accumulator), {});
